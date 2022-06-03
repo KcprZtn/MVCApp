e242d644689dd87dd2fd5507bc1ef49116daab61
KcprZtn <kcprzaton@gmail.com>
2022-06-03 21:51:07 +0200

    Fixed creating, editing and deleting. Trying to connect database

--- LearningMVC/Controllers/HomeController.cs
+++ LearningMVC/Controllers/HomeController.cs
@@ -1,96 +1,103 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LearningMVC.Models;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using LearningMVC.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.Sql;
+using Microsoft.Data.SqlClient;
+
 namespace LearningMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration configuration;
+
+        public  HomeController(IConfiguration config)
+        {
+            this.configuration = config;
+        }
+       
         private static IList<PetModel> pets = new List<PetModel>()
         {
-            new PetModel(){Name = "Feliks", Type = "Cat", BirthYear = 2015, Alive = true},
-            new PetModel(){Name = "Mazekin", Type = "Dog", BirthYear = 2020, Alive = true},
-            new PetModel(){Name = "Aniela", Type = "Cat", BirthYear = 2010, Alive = true},
-            new PetModel(){Name = "Hiszpan", Type = "Horse", BirthYear = 1999, Alive = true},
-            new PetModel(){Name = "Alex", Type = "Dog", BirthYear = 2004, Alive = false}
+
+            new PetModel(){Id = 1, Name = "Feliks", Type = "Cat", BirthYear = 2015, isAlive = true},
+            new PetModel(){Id = 2, Name = "Mazekin", Type = "Dog", BirthYear = 2020, isAlive = true},
+            new PetModel(){Id = 3, Name = "Aniela", Type = "Cat", BirthYear = 2010, isAlive = true},
+            new PetModel(){Id = 4, Name = "Hiszpan", Type = "Horse", BirthYear = 1999, isAlive = true},
+            new PetModel(){Id = 5, Name = "Alex", Type = "Dog", BirthYear = 2004, isAlive = false}
         };
         // GET: HomeController
         public ActionResult Index()
         {
+            string connectionstring = configuration.GetConnectionString("DefaultConnection");
+            SqlConnection connection = new SqlConnection(connectionstring);
+            connection.Open();
+            SqlCommand com = new SqlCommand("SELECT COUNT(*) FROM Pets;");
+            var count = (int)com.ExecuteScalar();
+            ViewData["TestData"] = count;
+            connection.Close();
             return View();
         }
 
         // GET: HomeController/Details/5
         public ActionResult Pets()
         {
-            
+
             return View(pets);
         }
 
         // GET: HomeController/Create
         public ActionResult Create()
         {
-            return View();
+            return View(new PetModel());
         }
 
         // POST: HomeController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(PetModel petmodel)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            petmodel.Id = pets.Count + 1;
+            pets.Add(petmodel);
+            return RedirectToAction(nameof(Index));
+
+
         }
 
         // GET: HomeController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            return View(pets.FirstOrDefault(x => x.Id == id));
         }
 
         // POST: HomeController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, PetModel petmodel)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            PetModel pet = pets.FirstOrDefault(x => x.Id == id);
+            pet.Name = petmodel.Name;
+            pet.BirthYear = petmodel.BirthYear;
+            pet.isAlive = petmodel.isAlive;
+           
+            return RedirectToAction(nameof(Index));
+
+
         }
 
         // GET: HomeController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            return View(pets.FirstOrDefault(x => x.Id == id));
         }
 
         // POST: HomeController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, PetModel petmodel)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            pets.Remove(pets.FirstOrDefault(x => x.Id == id));
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ LearningMVC/Models/PetModel - Copy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LearningMVC.Models
+{
+    public class LoginModel
+    {
+   
+        [Required(ErrorMessage = "Please enter login.")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Please enter your password.")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Your password needs to be 6-100 characters long.")]
+        public string Password { get; set; }
+
+    }
+}
