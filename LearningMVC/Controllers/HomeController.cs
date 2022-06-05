409100619771a0a58c19686d4d90022b3d8c81e3
KcprZtn <kcprzaton@gmail.com>
2022-06-05 20:48:42 +0200

    Connected database, fixed bugs

--- LearningMVC/Controllers/HomeController.cs
+++ LearningMVC/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Data.Sql;
 using Microsoft.Data.SqlClient;
 
 namespace LearningMVC.Controllers
@@ -11,37 +10,62 @@ namespace LearningMVC.Controllers
     public class HomeController : Controller
     {
         private readonly IConfiguration configuration;
-
+        
         public  HomeController(IConfiguration config)
         {
             this.configuration = config;
         }
-       
-        private static IList<PetModel> pets = new List<PetModel>()
-        {
+        
+        private static IList<PetModel> pets = new List<PetModel>(){};
 
-            new PetModel(){Id = 1, Name = "Feliks", Type = "Cat", BirthYear = 2015, isAlive = true},
-            new PetModel(){Id = 2, Name = "Mazekin", Type = "Dog", BirthYear = 2020, isAlive = true},
-            new PetModel(){Id = 3, Name = "Aniela", Type = "Cat", BirthYear = 2010, isAlive = true},
-            new PetModel(){Id = 4, Name = "Hiszpan", Type = "Horse", BirthYear = 1999, isAlive = true},
-            new PetModel(){Id = 5, Name = "Alex", Type = "Dog", BirthYear = 2004, isAlive = false}
-        };
         // GET: HomeController
         public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+        // GET: HomeController/Pets
+        public ActionResult Pets()
         {
             string connectionstring = configuration.GetConnectionString("DefaultConnection");
             SqlConnection connection = new SqlConnection(connectionstring);
             connection.Open();
-            SqlCommand com = new SqlCommand("SELECT COUNT(*) FROM Pets;");
-            var count = (int)com.ExecuteScalar();
-            ViewData["TestData"] = count;
-            connection.Close();
-            return View();
-        }
+            SqlCommand com = new SqlCommand("SELECT MAX(Id) FROM Pets;", connection);
+            var max = (int)com.ExecuteScalar();
 
-        // GET: HomeController/Details/5
-        public ActionResult Pets()
-        {
+            if (!(pets.Count > 0))
+            {
+                for (int i = 1; i <= max; i++)
+                {
+                    try
+                    {
+                        SqlCommand IdCom = new SqlCommand($"SELECT Id FROM Pets WHERE Id = {i}", connection);
+                        SqlCommand NameCom = new SqlCommand($"SELECT Name FROM Pets WHERE Id = {i}", connection);
+                        SqlCommand TypeCom = new SqlCommand($"SELECT Type FROM Pets WHERE Id = {i}", connection);
+                        SqlCommand BirthCom = new SqlCommand($"SELECT BirthYear FROM Pets WHERE Id = {i}", connection);
+                        SqlCommand AliveCom = new SqlCommand($"SELECT isAlive FROM Pets WHERE Id = {i}", connection);
+
+                        var pId = (int)IdCom.ExecuteScalar();
+                        var pName = (string)NameCom.ExecuteScalar();
+                        var pType = (string)TypeCom.ExecuteScalar();
+                        var pBirth = (int)BirthCom.ExecuteScalar();
+                        var pAlive = (bool)AliveCom.ExecuteScalar();
+
+                        pets.Add(new PetModel() { Id = pId, Name = pName, Type = pType, BirthYear = pBirth, isAlive = pAlive });
+                        
+                    }
+                    catch (System.Exception e)
+                    {
+                        
+
+                    }
+
+                }
+            }
+         
+            connection.Close();
+
 
             return View(pets);
         }
@@ -57,9 +81,15 @@ namespace LearningMVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(PetModel petmodel)
         {
-            petmodel.Id = pets.Count + 1;
+            string connectionstring = configuration.GetConnectionString("DefaultConnection");
+            SqlConnection connection = new SqlConnection(connectionstring);
+            connection.Open();
+            SqlCommand createCom = new SqlCommand($"INSERT INTO Pets(Name,Type,BirthYear,isAlive) VALUES('{petmodel.Name}','{petmodel.Type}',{petmodel.BirthYear},'{petmodel.isAlive}');", connection);
+            createCom.ExecuteScalar();
             pets.Add(petmodel);
-            return RedirectToAction(nameof(Index));
+            connection.Close();
+           
+            return RedirectToAction(nameof(Pets));
 
 
         }
@@ -75,12 +105,31 @@ namespace LearningMVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, PetModel petmodel)
         {
-            PetModel pet = pets.FirstOrDefault(x => x.Id == id);
-            pet.Name = petmodel.Name;
-            pet.BirthYear = petmodel.BirthYear;
-            pet.isAlive = petmodel.isAlive;
+
+            try
+            {
+                PetModel pet = pets.FirstOrDefault(x => x.Id == id);
+                pet.Name = petmodel.Name;
+                pet.Type = petmodel.Type;
+                pet.BirthYear = petmodel.BirthYear;
+                pet.isAlive = petmodel.isAlive;
+
+
+                string connectionstring = configuration.GetConnectionString("DefaultConnection");
+                SqlConnection connection = new SqlConnection(connectionstring);
+
+                connection.Open();
+                SqlCommand createCom = new SqlCommand($"UPDATE Pets SET Name = '{petmodel.Name}', Type = '{petmodel.Type}', BirthYear = {petmodel.BirthYear}, isAlive = '{petmodel.isAlive}'  WHERE Id = {id};", connection);
+                createCom.ExecuteScalar();
+                connection.Close();
+            }
+            catch (System.Exception e)
+            {
+               
+                
+            }
            
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction(nameof(Pets));
 
 
         }
@@ -96,8 +145,15 @@ namespace LearningMVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, PetModel petmodel)
         {
+            string connectionstring = configuration.GetConnectionString("DefaultConnection");
+            SqlConnection connection = new SqlConnection(connectionstring);
+            connection.Open();
+            SqlCommand deleteCom = new SqlCommand($"DELETE FROM Pets WHERE Id = {id}",connection);
+            deleteCom.ExecuteScalar();
             pets.Remove(pets.FirstOrDefault(x => x.Id == id));
-            return RedirectToAction(nameof(Index));
+
+            connection.Close();
+            return RedirectToAction(nameof(Pets));
         }
     }
 }
